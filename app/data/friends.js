deccf676201f6b8778d96db9164f1a2ec5b9652a
unknown <Joseph.Kampa@gmail.com>
2018-06-10 11:06:00 -0500

    Fake Data Added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,24 @@
 //saved array data
+
+var friendList = [{
+    name: "Lucky Boy",
+    photo: "https://www.kimballstock.com/images/dog-stock-photos.jpg",
+    scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
+}, {
+    name: "Goofy Tails",
+    photo: "http://blog.123rf.com/wp-content/uploads/2015/03/37304620_m.jpg",
+    scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+}, {
+    name: "Sketchy Hands",
+    photo: "https://thumbs.dreamstime.com/b/cartoon-thief-running-vector-illustration-32465599.jpg",
+    scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+}, {
+    name: "Timid Paws",
+    photo: "https://image1.masterfile.com/getImage/NjIyLTA2ODQyMTU1ZW4uMDAwMDAwMDA=AKwFdN/622-06842155en_Masterfile.jpg",
+    scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+}];
+
+module.exports = friendList;
+
+
+//all above photos are from free stock photo websites
