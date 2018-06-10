58883b1fbeadfea95cf3398f35b32f96a9594897
unknown <Joseph.Kampa@gmail.com>
2018-06-10 06:37:41 -0500

    Added Routes, updated Route to HTML pages

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,43 @@
 //api routing
+
+//required file
+var friendList = require('../data/friend.js');
+
+//get route
+module.exports = function (app) {
+    app.get('/api/friends', function (req, res) {
+        res.json(friendList);
+    });
+
+    //post route
+    app.post('/api/friends', function (req, res) {
+
+        var newFriendScores = req.body.scores;
+        var scoresArray = [];
+        var friendCount = 0;
+        var bestMatch = 0;
+
+        //compare scores against preloaded/already done users
+        for (var i = 0; i < friendList.length; i++) {
+            var scoresDiff = 0;
+            for (var j = 0; j < newFriendScores.length; j++) {
+                scoresDiff += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newFriendScores[j])));
+            }
+            scoresArray.push(scoresDiff);
+        }
+
+        //find best match to be returned/displayed
+        for (var i = 0; i < scoresArray.length; i++) {
+            if (scoresArray[i] <= scoresArray[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+
+        //output to modal
+        var bff = friendList[bestMatch];
+        res.json(bff);
+
+        //add to arry
+        friendList.push(req.body);
+    });
+};
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,1 +1,20 @@
 //html routing
+
+//required npm
+var path = require("path");
+
+//routes
+module.exports = function (app) {
+
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
